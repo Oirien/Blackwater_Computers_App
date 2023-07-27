06d9856ff647dab056dced4b71c6f0d0045bbc7c
Oirien <robert.mayo.ch@gmail.com>
2023-07-27 21:36:34 +0100

    fixed database seeding issues

--- models/builds.py
+++ models/builds.py
@@ -4,5 +4,5 @@ class Build(db.Model):
     __tablename__ = "builds"
 
     id = db.Column(db.Integer, primary_key=True)
-    componentlist = db.relationship('componentlists', backref='builds')
+    componentlist = db.relationship('ComponentList', backref='builds')
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
