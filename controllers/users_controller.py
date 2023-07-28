a29bae2e02f449c2b92cf73054fedb04be0cf21b
Oirien <robert.mayo.ch@gmail.com>
2023-07-28 07:34:12 +0100

    started some basic routing and funcitonality within users/users_controller

--- controllers/users_controller.py
+++ controllers/users_controller.py
@@ -1,6 +1,20 @@
 from flask import Flask, render_template, request, redirect
 from flask import Blueprint
 from models.users import User
+from models.builds import Build
+from models.components.component import Component
 from app import db
 
 users_blueprint = Blueprint("users", __name__)
+
+@users_blueprint.route("/users")
+def users():
+    users = User.query.all()
+    return render_template("users/index.jinja", users=users)
+
+@users_blueprint.route("/users/<id>")
+def single_user(id):
+    user = User.query.get(id)
+    builds = Build.query.all()
+    components = Component.query.all()
+    return render_template("users/show.jinja", user=user, builds=builds, components=components)
