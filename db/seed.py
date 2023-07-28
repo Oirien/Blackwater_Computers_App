3c4b388b4fdbed37731465e811b54ee4c375db16
Oirien <robert.mayo.ch@gmail.com>
2023-07-28 19:33:54 +0100

    Slowly working on seeding refactoring

--- db/seed.py
+++ db/seed.py
@@ -30,9 +30,9 @@ def seed():
     db.session.add_all([user1, user2, user3])
     db.session.commit()
 
-    motherboard1 = Motherboard(name="ASUS Prime Z590-A", description="This is a motherboard description", image_link="This is a motherboard picture", type="Motherboard", price=200, manufacturer="ASUS", power_draw=20, chipset="Z590")
-    motherboard2 = Motherboard(name="GIGABYTE B550 AORUS Elite", description="This is a motherboard description", image_link="This is a motherboard picture", type="Motherboard", price=150, manufacturer="GIGABYTE", power_draw=25, chipset="B550")
-    motherboard3 = Motherboard(name="MSI B450 TOMAHAWK MAX", description="This is a motherboard description", image_link="This is a motherboard picture", type="Motherboard", price=120, manufacturer="MSI", power_draw=18, chipset="B450")
+    motherboard1 = Motherboard(name="ASUS Prime Z590-A", description="This is a motherboard description", image_link="This is a motherboard picture", type="Motherboard", price=200, manufacturer="ASUS", power_draw=20, chipset="LGA1200")
+    motherboard2 = Motherboard(name="GIGABYTE B550 AORUS Elite", description="This is a motherboard description", image_link="This is a motherboard picture", type="Motherboard", price=150, manufacturer="GIGABYTE", power_draw=25, chipset="AM4")
+    motherboard3 = Motherboard(name="MSI B450 TOMAHAWK MAX", description="This is a motherboard description", image_link="This is a motherboard picture", type="Motherboard", price=120, manufacturer="MSI", power_draw=18, chipset="AM4")
     db.session.add_all([motherboard1, motherboard2, motherboard3])
     db.session.commit()
 
@@ -40,4 +40,16 @@ def seed():
     cpu2 = Cpu(name="AMD Ryzen 9 5950X", description="This is a CPU description", image_link="This is a cpu picture", type="CPU", price=400, manufacturer="AMD", power_draw=120, chipset="AM4")
     cpu3 = Cpu(name="Intel Core i5-12600K", description="This is a CPU description", image_link="This is a cpu picture", type="CPU", price=300, manufacturer="Intel", power_draw=100, chipset="LGA 1200")
     db.session.add_all([cpu1, cpu2, cpu3])
+    db.session.commit()
+
+    gpu1 = Gpu(name="NVIDIA GeForce RTX 4080ti", description="This is a GPU description", image_link="This is a gpu picture", type="GPU", price=800, manufacturer="NVIDIA", power_draw=320)
+    gpu2 = Gpu(name="AMD Radeon RX 7900 XTX", description="This is a GPU description", image_link="This is a gpu picture", type="GPU", price=700, manufacturer="AMD", power_draw=300)
+    gpu3 = Gpu(name="NVIDIA GeForce RTX 4070", description="This is a GPU description", image_link="This is a gpu picture", type="GPU", price=600, manufacturer="NVIDIA", power_draw=280)
+    db.session.add_all([gpu1, gpu2, gpu3])
+    db.session.commit()
+
+    case1 = Case(name="NZXT H510", description="This is a case description", image_link="This is a case picture", type="Case", price=80, manufacturer="NZXT")
+    case2 = Case(name="Corsair iCUE 4000X RGB", description="This is a case description", image_link="This is a case picture", type="Case", price=100, manufacturer="Corsair")
+    case3 = Case(name="Lian Li O11 DYNAMIC", description="This is a case description", image_link="This is a case picture", type="Case", price=90, manufacturer="Lian Li")
+    db.session.add_all([case1, case2, case3])
     db.session.commit()
