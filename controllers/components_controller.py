e6ba42be4ebf0eb3d0d157c1e6c7879f1a3eaa62
oirien <robert.mayo.ch@gmail.com>
2023-07-28 11:34:32 +0100

    controllers and jinja for components started

--- controllers/components_controller.py
+++ controllers/components_controller.py
@@ -4,3 +4,13 @@ from models.components.component import Component
 from app import db
 
 components_blueprint = Blueprint("components", __name__)
+
+@components_blueprint.route("/components")
+def components():
+    components = Component.query.all()
+    return render_template("components/index.jinja", components=components)
+
+@components_blueprint.route("/components/<type>")
+def component_type(type):
+    component_type = Component.query.filter_by(type = type)
+    return render_template("components/sort.jinja", components=component_type)
