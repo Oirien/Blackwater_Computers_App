1b0f686c64ddee2b62ad0c91054cc362549fea17
Oirien <robert.mayo.ch@gmail.com>
2023-07-29 09:24:58 +0100

    Slowly working on seeding refactoring still

--- db/seed.py
+++ db/seed.py
@@ -30,6 +30,18 @@ def seed():
     db.session.add_all([user1, user2, user3])
     db.session.commit()
 
+    case1 = Case(name="NZXT H510", description="This is a case description", image_link="This is a case picture", type="Case", price=80, manufacturer="NZXT")
+    case2 = Case(name="Corsair iCUE 4000X RGB", description="This is a case description", image_link="This is a case picture", type="Case", price=100, manufacturer="Corsair")
+    case3 = Case(name="Lian Li O11 DYNAMIC", description="This is a case description", image_link="This is a case picture", type="Case", price=90, manufacturer="Lian Li")
+    db.session.add_all([case1, case2, case3])
+    db.session.commit()
+
+    psu1 = Psu(name="EVGA SuperNOVA 850 G5", description="This is a PSU description", image_link="This is a psu picture", type="PSU", price=150, manufacturer="EVGA", power_supply=850)
+    psu2 = Psu(name="Corsair RM750x", description="This is a PSU description", image_link="This is a psu picture", type="PSU", price=120, manufacturer="Corsair", power_supply=750)
+    psu3 = Psu(name="Seasonic Focus GX-650", description="This is a PSU description", image_link="This is a psu picture", type="PSU", price=100, manufacturer="Seasonic", power_supply=650)
+    db.session.add_all([psu1, psu2, psu3])
+    db.session.commit()
+
     motherboard1 = Motherboard(name="ASUS Prime Z590-A", description="This is a motherboard description", image_link="This is a motherboard picture", type="Motherboard", price=200, manufacturer="ASUS", power_draw=20, chipset="LGA1200")
     motherboard2 = Motherboard(name="GIGABYTE B550 AORUS Elite", description="This is a motherboard description", image_link="This is a motherboard picture", type="Motherboard", price=150, manufacturer="GIGABYTE", power_draw=25, chipset="AM4")
     motherboard3 = Motherboard(name="MSI B450 TOMAHAWK MAX", description="This is a motherboard description", image_link="This is a motherboard picture", type="Motherboard", price=120, manufacturer="MSI", power_draw=18, chipset="AM4")
@@ -42,14 +54,14 @@ def seed():
     db.session.add_all([cpu1, cpu2, cpu3])
     db.session.commit()
 
+    cooler1 = Cpu_Cooler(name="Noctua NH-D15", description="This is a CPU Cooler description", image_link="This is a cooler picture", type="CPU Cooler", price=100, manufacturer="Noctua")
+    cooler2 = Cpu_Cooler(name="be quiet! Dark Rock Pro 4", description="This is a CPU Cooler description", image_link="This is a cooler picture", type="CPU Cooler", price=90, manufacturer="be quiet!")
+    cooler3 = Cpu_Cooler(name="Cooler Master Hyper 212 EVO", description="This is a CPU Cooler description", image_link="This is a cooler picture", type="CPU Cooler", price=40, manufacturer="Cooler Master")
+    db.session.add_all([cooler1, cooler2, cooler3])
+    db.session.commit()
+
     gpu1 = Gpu(name="NVIDIA GeForce RTX 4080ti", description="This is a GPU description", image_link="This is a gpu picture", type="GPU", price=800, manufacturer="NVIDIA", power_draw=320)
     gpu2 = Gpu(name="AMD Radeon RX 7900 XTX", description="This is a GPU description", image_link="This is a gpu picture", type="GPU", price=700, manufacturer="AMD", power_draw=300)
     gpu3 = Gpu(name="NVIDIA GeForce RTX 4070", description="This is a GPU description", image_link="This is a gpu picture", type="GPU", price=600, manufacturer="NVIDIA", power_draw=280)
     db.session.add_all([gpu1, gpu2, gpu3])
-    db.session.commit()
-
-    case1 = Case(name="NZXT H510", description="This is a case description", image_link="This is a case picture", type="Case", price=80, manufacturer="NZXT")
-    case2 = Case(name="Corsair iCUE 4000X RGB", description="This is a case description", image_link="This is a case picture", type="Case", price=100, manufacturer="Corsair")
-    case3 = Case(name="Lian Li O11 DYNAMIC", description="This is a case description", image_link="This is a case picture", type="Case", price=90, manufacturer="Lian Li")
-    db.session.add_all([case1, case2, case3])
     db.session.commit()
