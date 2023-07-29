00205ec2e166dae93fd1d14bf74dd9d8f8b6460f
Oirien <robert.mayo.ch@gmail.com>
2023-07-29 16:32:29 +0100

    refactored the components controller and templates

--- controllers/components_controller.py
+++ controllers/components_controller.py
@@ -7,15 +7,16 @@ components_blueprint = Blueprint("components", __name__)
 
 @components_blueprint.route("/components")
 def components():
-    components = Component.query.all()
-    return render_template("components/index.jinja", components=components)
+    component_types = Component.query.with_entities(Component.type).distinct().all()
+    return render_template("components/index.jinja", component_types=component_types)
 
-@components_blueprint.route("/components/<type>")
+
+@components_blueprint.route("/components/sort/<type>")
 def component_type(type):
-    component_type = Component.query.filter_by(type = type)
-    return render_template("components/sort.jinja", components=component_type)
+    components = Component.query.filter_by(type=type).all()
+    return render_template("components/sort.jinja", components=components, type=type)
 
-@components_blueprint.route("/components/sort/<id>")
+@components_blueprint.route("/components/show/<id>")
 def component_show(id):
     component = Component.query.get(id)
     return render_template("components/show.jinja", component=component)
